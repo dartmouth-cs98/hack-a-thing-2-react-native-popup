d63f3560f885e9fdb50008ff123fb85a9a3d6b56
emmaxs <emmaxs@users.noreply.github.com>
2020-01-21 16:21:03 -0500

    Added new react-navigation-tabs library to fix crash bug

--- my-new-project/navigation/main_tab_bar.js
+++ my-new-project/navigation/main_tab_bar.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { View, Text } from 'react-native';
 
-const AboutTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>about</Text></View>;
+const AboutTab = props => {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center' }}>
+			<Text>about</Text>
+		</View>
+	);
 };
 
-const SearchTab = (props) => {
-  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Search</Text></View>;
+const SearchTab = props => {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center' }}>
+			<Text>Search</Text>
+		</View>
+	);
 };
 
-
-const MainTabBar = createBottomTabNavigator({
-  SearchTab,
-  AboutTab,
-}, {
-  initialRouteName: 'SearchTab',
-});
-
+const MainTabBar = createBottomTabNavigator(
+	{
+		SearchTab,
+		AboutTab,
+	},
+	{
+		initialRouteName: 'SearchTab',
+	}
+);
 
 export default createAppContainer(MainTabBar);
