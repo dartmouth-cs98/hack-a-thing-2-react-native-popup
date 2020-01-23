9f63622718f555f69be68b5d420c3b3965db7384
emmaxs <emmaxs@users.noreply.github.com>
2020-01-23 17:37:03 -0500

    Added stack navigator to search tab and installed dependencies not mentioned in the tutorial

--- my-new-project/navigation/main_tab_bar.js
+++ my-new-project/navigation/main_tab_bar.js
@@ -3,23 +3,24 @@ import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import Ionicons from 'react-native-vector-icons/FontAwesome';
 import About from '../components/about';
+import SearchTab from './search_tab';
 import { View, Text } from 'react-native';
 
-const AboutTab = props => {
-	return (
-		<View style={{ flex: 1, justifyContent: 'center' }}>
-			<Text>about</Text>
-		</View>
-	);
-};
+// const AboutTab = props => {
+// 	return (
+// 		<View style={{ flex: 1, justifyContent: 'center' }}>
+// 			<Text>about</Text>
+// 		</View>
+// 	);
+// };
 
-const SearchTab = props => {
-	return (
-		<View style={{ flex: 1, justifyContent: 'center' }}>
-			<Text>Search</Text>
-		</View>
-	);
-};
+// const SearchTab = props => {
+// 	return (
+// 		<View style={{ flex: 1, justifyContent: 'center' }}>
+// 			<Text>Search</Text>
+// 		</View>
+// 	);
+// };
 
 const MainTabBar = createBottomTabNavigator(
 	{
