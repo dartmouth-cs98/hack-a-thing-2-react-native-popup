92112e1393df7ecfaf2ff6acfde2dfc44fac589c
David <david.mena.20@dartmouth.edu>
2020-01-26 14:33:21 -0500

    Working on different sample layouts

--- my-new-project/navigation/main_tab_bar.js
+++ my-new-project/navigation/main_tab_bar.js
@@ -1,42 +1,22 @@
 import React from 'react';
-import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
-import Ionicons from 'react-native-vector-icons/FontAwesome';
-import About from '../components/about';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 import { View, Text } from 'react-native';
 
-const AboutTab = props => {
-	return (
-		<View style={{ flex: 1, justifyContent: 'center' }}>
-			<Text>about</Text>
-		</View>
-	);
+const AboutTab = (props) => {
+  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>about</Text></View>;
 };
 
-const SearchTab = props => {
-	return (
-		<View style={{ flex: 1, justifyContent: 'center' }}>
-			<Text>Search</Text>
-		</View>
-	);
+const SearchTab = (props) => {
+  return <View style={{ flex: 1, justifyContent: 'center' }}><Text>Search</Text></View>;
 };
 
-const MainTabBar = createBottomTabNavigator(
-	{
-		SearchTab,
-		AboutTab: {
-			screen: About,
-			navigationOptions: ({ navigation }) => ({
-				tabBarLabel: 'About',
-				tabBarIcon: ({ focused }) => (
-					<Ionicons name="info-circle" size={26} color={focused ? '#58AADA' : 'grey'} />
-				),
-			}),
-		},
-	},
-	{
-		initialRouteName: 'SearchTab',
-	}
-);
+
+const MainTabBar = createBottomTabNavigator({
+  SearchTab,
+  AboutTab,
+}, {
+  initialRouteName: 'SearchTab',
+});
+
 
 export default createAppContainer(MainTabBar);
